b317faafbb85d618578e68ea678c86bf87de0016
Dalton Spillman <dalton_spillman@yahoo.com>
2019-02-09 16:27:40 -0600

    Created implicit operators for NullableString

---
+++ Unofficial.SignalR.Protobuf/ProtobufNullablePartials.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unofficial.SignalR.Protobuf
+{
+    public partial class NullableString
+    {
+        public static implicit operator string(NullableString nullableProto)
+        {
+            return nullableProto?.Value;
+        }
+
+        public static implicit operator NullableString(string value)
+        {
+            return value == null
+                ? null
+                : new NullableString { Value = value };
+        }
+    }
+}
