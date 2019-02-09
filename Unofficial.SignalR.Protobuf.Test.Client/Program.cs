8258d9ae7db7f26c43468635895aeeafaa15dae2
Dalton Spillman <dalton_spillman@yahoo.com>
2019-02-08 18:46:26 -0600

    Client can now connect to the localhost server

--- Unofficial.SignalR.Protobuf.Test.Client/Program.cs
+++ Unofficial.SignalR.Protobuf.Test.Client/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Unofficial.SignalR.Protobuf.Test.Core;
 
 namespace Unofficial.SignalR.Protobuf.Test.Client
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HubConnectionBuilder()
+                .WithUrl("http://localhost:51429/realtime")
+                .AddProtobufProtocol(MessagesReflection.Descriptor.MessageTypes)
+                .Build();
+
+            await client.StartAsync();
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
         }
     }
 }
