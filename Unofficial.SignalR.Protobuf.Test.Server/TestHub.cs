9ac7afbe6daa045fcdad01d8abe7e0e7a9de02f2
Dalton Spillman <dalton_spillman@yahoo.com>
2019-02-08 19:10:34 -0600

    Completed tests for showing the problems with custom protocols

--- Unofficial.SignalR.Protobuf.Test.Server/TestHub.cs
+++ Unofficial.SignalR.Protobuf.Test.Server/TestHub.cs
@@ -1,15 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using Unofficial.SignalR.Protobuf.Test.Core;
 
 namespace Unofficial.SignalR.Protobuf.Test.Server
 {
     public class TestHub : Hub
     {
-        public override Task OnConnectedAsync()
+        public override async Task OnConnectedAsync()
         {
+            Debug.WriteLine("Client connected!");
+            
+            await Clients.Caller.SendAsync(
+                "HandleTestMessage", 
+                new TestMessage { Value = "This came from the server!" }
+            );
 
+            await base.OnConnectedAsync();
+        }
 
-            return base.OnConnectedAsync();
+        public Task HandleTestMessage(TestMessage message)
+        {
+            Debug.WriteLine($"Server received \"{message.Value}\"");
+
+            return Task.CompletedTask;
         }
     }
 }
