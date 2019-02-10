498c22b3ccb01a23426866cca5fc8f1b8631207b
Dalton Spillman <dalton_spillman@yahoo.com>
2019-02-09 19:27:37 -0600

    Refactoring serializing/deserializing using IMessageSerializer

---
+++ Unofficial.SignalR.Protobuf/MessageSerializers/IMessageSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR.Protocol;
+
+namespace Unofficial.SignalR.Protobuf.MessageSerializers
+{
+    public interface IMessageSerializer
+    {
+        IEnumerable<byte> SupportedTypeBytes { get; }
+        IEnumerable<Type> SupportedTypes { get; }
+        byte GetTypeByte(HubMessage message);
+        void WriteMessage(HubMessage message, IBufferWriter<byte> output, IReadOnlyDictionary<Type, int> protobufTypeToIndexMap);
+        bool TryParseMessage(ref ReadOnlySequence<byte> input, out HubMessage message, IReadOnlyList<Type> protobufTypes);
+    }
+}
