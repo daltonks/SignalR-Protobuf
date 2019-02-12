a8835e86cf1ec3f82dd19eb812fc6f5135ec285a
Dalton Spillman <dalton_spillman@yahoo.com>
2019-02-12 00:45:34 -0600

    Might be done with serializers for all message types?

---
+++ Unofficial.SignalR.Protobuf/MessageSerializers/CompletionMessageSerializer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Protobuf;
+using Microsoft.AspNetCore.SignalR.Protocol;
+using Unofficial.SignalR.Protobuf.MessageSerializers.Base;
+
+namespace Unofficial.SignalR.Protobuf.MessageSerializers
+{
+    public class CompletionMessageSerializer : BaseMessageSerializer
+    {
+        public override ProtobufMessageType EnumType => ProtobufMessageType.Completion;
+        public override Type MessageType => typeof(CompletionMessage);
+
+        protected override IEnumerable<IMessage> CreateProtobufModels(HubMessage message)
+        {
+            var completionMessage = (CompletionMessage) message;
+
+            yield return new CompletionMessageProtobuf
+            {
+                InvocationId = completionMessage.InvocationId,
+                Headers = {completionMessage.Headers.Flatten()},
+                Error = completionMessage.Error,
+                HasResult = completionMessage.HasResult
+            };
+
+            yield return (IMessage) completionMessage.Result;
+        }
+
+        protected override HubMessage CreateHubMessage(IReadOnlyList<IMessage> protobufModels)
+        {
+            var protobuf = (CompletionMessageProtobuf) protobufModels.First();
+            var resultProtobuf = protobufModels[1];
+
+            return new CompletionMessage(
+                protobuf.InvocationId, 
+                protobuf.Error, 
+                resultProtobuf, 
+                protobuf.HasResult
+            );
+        }
+    }
+}
