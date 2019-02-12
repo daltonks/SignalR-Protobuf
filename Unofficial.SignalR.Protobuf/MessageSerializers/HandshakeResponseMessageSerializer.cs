29e5a724198b3658fcbc5c28a294e3b480f70ec5
Dalton Spillman <dalton_spillman@yahoo.com>
2019-02-12 00:29:01 -0600

    Created HandshakeRequestMessageProtobuf and serializer

--- Unofficial.SignalR.Protobuf/MessageSerializers/HandshakeResponseMessageSerializer.cs
+++ Unofficial.SignalR.Protobuf/MessageSerializers/HandshakeResponseMessageSerializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Google.Protobuf;
 using Microsoft.AspNetCore.SignalR.Protocol;
+using Unofficial.SignalR.Protobuf.MessageSerializers.Base;
 
 namespace Unofficial.SignalR.Protobuf.MessageSerializers
 {
--- Unofficial.SignalR.Protobuf/MessageSerializers/PingMessageSerializer.cs
+++ Unofficial.SignalR.Protobuf/MessageSerializers/PingMessageSerializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Google.Protobuf;
 using Microsoft.AspNetCore.SignalR.Protocol;
+using Unofficial.SignalR.Protobuf.MessageSerializers.Base;
 
 namespace Unofficial.SignalR.Protobuf.MessageSerializers
 {
